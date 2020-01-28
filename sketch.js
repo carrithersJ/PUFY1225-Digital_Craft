26633b7180d7337385c08acc776d3a26d226756d
carrithersJ <60146931+carrithersJ@users.noreply.github.com>
2020-01-28 11:40:31 -0500

    template

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("#0");
+  //ellipse(x position, y position, width, height)
+  fill("#d9d9d9");
+  rect(380,0,390,320);
+  rect(0,0,140,40);
+  fill("#ff3333");
+rect(60,40, 200, 200);
+  strokeWeight(5);
+rect(380,310,90,90);
+  fill("#ffff66");
+rect(0,290,40,150);
+  rect(230,0,140,38);
+  fill("#000000");
+       rect(60,240,80,100);
+}
